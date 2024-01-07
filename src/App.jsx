05f7a8a61a040084ce4fb9d40bca541db92895c8
gyanbahadursunuwar <gyanbahadursunuwar@gmail.com>
2024-01-07 15:29:45 +0530

    Modified as listing of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import Counter from './counter.jsx'
+import Counter from './apps/counter.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [counterVisible, setCounterVisible]= useState(false)
+  const handleCounterOnClick = () =>{
+    setCounterVisible(!counterVisible);
+  }
   return (
     <>
-    <h1> Welcome to the Vite@React</h1>
-      <Counter />
+    <h1> Welcome to the React </h1>
+    <h2>Please choose any app.</h2>
+    <a href='#' onClick={handleCounterOnClick}>Counter</a> <br/>
+    {counterVisible && <Counter/>}
     </>
   )
 }
